d52351853983f19de1618784e1b6049fe2a27a2e
Chris Maltais <chris.maltais@hotmail.ca>
2018-11-07 12:11:55 -0500

    Fix GET /members to GET /api/members by fixing logic in member.controller.js and member.routes.js

--- server/api/routes/member.routes.js
+++ server/api/routes/member.routes.js
@@ -4,15 +4,13 @@ module.exports = (controller) => {
     const memberAPI = Router();
     const { member } = controller;
 
+    // Note, these routes should just return status codes, heavy lifting of logic is done in the controller!
     memberAPI.get('/members', (req, res) => {
         member
             .getAllMembers().then((members) => {
-            res.send({
-                members, // This is what your object will be called!
-                message: 'This is a test!'
-            })
-        }, (err) => {
-            res.status(400).send(err);
+                res.status(200).send(members); // members object is structured the way we want it in controller!
+            }, (err) => {
+                res.status(400).send(err);
         });
     });
 
--- server/controllers/member.controller.js
+++ server/controllers/member.controller.js
@@ -5,7 +5,10 @@ module.exports = {
         return new Promise((resolve, reject) => {
             Member.find()
                 .then((members) => {
-                    resolve({members});
+                    resolve({
+                        members,
+                        message: "This is coming from member.controller.js"
+                    });
                 })
                 .catch((err) => {
                     reject('Could not retrieve all members!');
